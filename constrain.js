074f6ea85f3cff17e30d51a74e501d5448b76c23
QuangHuy-3110 <quanghuy.0511204@gmail.com>
2024-10-19 18:24:25 +0700

    Update constrain.js

--- constrain.js
+++ constrain.js
@@ -46,10 +46,25 @@ let itemList = {
 };
 
 function move_donhang(){
-    window.location.href="donhang.html";
+    window.location.href="donhang.html";    
 }
 
+function int_to_tring (a){
+    let b = String(a);
+    let j = 0;
+    for(let i=b.length; i >= 0; i--){
+        if(j === 3 && i!==0){
+            b = b.substr(0, i) + '.' + b.substr(i);
+            j = 0;
+        }
+        j++;
+    }
+    return b;
+}
+
+
 function addcart (e) {
+    
     if(typeof localStorage[e] === "undefined"){
         number = parseInt(document.getElementById(e).value);
         window.localStorage.setItem(e, number);
@@ -73,8 +88,9 @@ function addcart (e) {
 }
 
 function show_cart() {
+    let total = 0;
     for (let key in localStorage){
-        console.log(localStorage[key]);
+        console.log(key);
         item = itemList[key];
         photo = item.photo;
         name = item.name;
@@ -83,7 +99,12 @@ function show_cart() {
 
         let tbody = document.getElementById("tbdy");
 
-        let tr = document.createElement("tr");
+        let tr = document.createElement("tr", id=`tr_${key}`);
+
+        let td_check = document.createElement("td");
+        td_check.innerHTML=`<input type="checkbox" id="box_${key}" class="checkbox" onclick="update_total1('${key}')">`;
+        tr.appendChild(td_check);
+
         let td_img = document.createElement("td");
         td_img.innerHTML=`<img src=${photo} width = '100px'>`
         tr.appendChild(td_img);
@@ -93,18 +114,141 @@ function show_cart() {
         tr.appendChild(td_name);
 
         let td_price = document.createElement("td");
-        td_price.innerHTML = price;
+        td_price.innerHTML = `${int_to_tring(price)}/chậu`;
         tr.appendChild(td_price);
 
         let td_orderNumber = document.createElement("td");
-        td_orderNumber.innerHTML = orderNumber;
+        td_orderNumber.innerHTML =`<input type="number" id="${key}_new" class="input_number" onchange="change_number('${key}')" name="soluong" max="100" min="0" size="3" value="${orderNumber}">` ;
         tr.appendChild(td_orderNumber);
+
+        let td_count = document.createElement("td");
+        td_count.innerHTML = int_to_tring (count_price(key));
+        tr.appendChild(td_count);
+
+        let td_delete = document.createElement("td");
+        td_delete.innerHTML = `<a class="icon_trash" onclick="removeCart('${key}')"><i class="fa-solid fa-trash-can"></i></a>`;
+        tr.appendChild(td_delete);
 
         tbody.append(tr);
     }
 }
-show_cart();
+
+function change_number (key){
+    let div_count = document.getElementById("count");
+    div_count.innerHTML = `<p>Vui lòng chọn sản phẩm trong giỏ hàng!</p>`;
+    let check = document.getElementById("check_all");
+    check.checked = false;
+    number = parseInt(document.getElementById(`${key}_new`).value);
+    if (number > 100){
+        window.localStorage.setItem(key, 100);
+        alert(`Sản phẩm ${itemList[key].name} đã đặt quá 100 sản phẩm!`);
+    }
+    else{
+        window.localStorage.setItem(key, number);
+    }
+    let tbody = document.getElementById("tbdy");
+    tbody.innerHTML ="";
+    update_total2()
+    show_cart();
+}
 
 
-// console.log(localStorage.length);
+let total = 0;
+function update_total1(key){
+    checkBox = document.getElementById(`box_${key}`);
+    if(checkBox.checked === true){
+        total += count_price(key);
+    }
+    else if(total > 0){
+        total -= count_price(key);
+    }
+    let discount = total*get_discount_rate();
+    let tax = (total - discount)*0.1;
+    let fee = total - discount + tax;
+    let div_count = document.getElementById("count");
+    if(fee !==0 ){
+        div_count.innerHTML = `<p class="count_fee">Tổng giá trị đơn hàng: <span>${int_to_tring(total)}đ</span> </p>
+                        <p class="count_fee">Giá trị khuyến mãi: <span>${int_to_tring(discount)}đ</span> </p>
+                        <p class="count_fee">Thuế giá trị gia tăng: <span>${int_to_tring(tax)}đ</span> </p>
+                        <p class="Buy">Thành tiền: <span class="fee">${int_to_tring(fee)}đ</span> </p>
+                        <button class="buy">Mua</button>`;   
+    }
+    else if(localStorage.length !== 0) {
+        div_count.innerHTML = `<p>Vui lòng chọn sản phẩm trong giỏ hàng!</p>`;
+    }
 
+}
+
+function check_all(){    
+    let check = document.getElementById("check_all");    
+    if (check.checked === true){  
+        update_total2();      
+        let checkbox = document.getElementsByClassName("checkbox");
+        for(let i in checkbox){
+            checkbox[i].checked = true;
+        }
+        for (let key in localStorage){
+            update_total1(key);
+        }
+    }
+    else {
+        let checkbox = document.getElementsByClassName("checkbox");
+        for(let i in checkbox){
+            checkbox[i].checked = false;
+        }
+        
+    }
+    update_total2();
+    let div_count = document.getElementById("count");
+    div_count.innerHTML = `<p>Vui lòng chọn sản phẩm trong giỏ hàng!</p>`;
+
+}
+
+
+function update_total2(){
+    total = 0;
+    // let div_count = document.getElementById("count");
+    // div_count.innerHTML = `<p>Vui lòng chọn sản phẩm trong giỏ hàng!</p>`;
+}
+
+function count_price(key){
+    item = itemList[key];
+    price = item.price;
+    orderNumber = localStorage.getItem(key);
+    return orderNumber*price;
+}
+
+function get_discount_rate(){
+    let d = new Date();
+    let weekday = d.getDay();
+    let totalMins = d.getHours()*60+d.getMinutes();
+    if(weekday >= 1  && weekday<=3 &&((totalMins >= 420 && totalMins <= 660) || (totalMins >= 780 && totalMins <= 1020))){
+        return 0.1;
+    }
+    return 0;
+}
+
+function display_cart() {
+    if (localStorage.length === 0){        
+        let a = document.getElementById("cart");
+        a.style.display = "none";
+        let div_count = document.getElementById("count");
+        div_count.innerHTML = `<p>Hiện tại chưa có sản phẩm nào trong giỏ hàng!</p>`;        
+    }
+    else{
+        let a = document.getElementById("cart");
+        a.style.display = "block";
+        show_cart();
+    }
+}
+
+display_cart();
+
+function removeCart (code){
+    update_total2();
+    if(typeof window.localStorage[code] !== "undefined"){
+        window.localStorage.removeItem(code);      
+        document.getElementById("CartDetail").getElementsByTagName('tbody')[0].innerHTML = "";
+        display_cart();        
+    }    
+}
